a38f6a1bfdab8c0c55d1129709291e0c3e4e88d6
Pavan Rikhi <pavan.rikhi@gmail.com>
2020-02-07 16:30:53 -0500

    Add Shopify Order Aggregate Type & Parsing Function
    Add a ShopifyOrder type for storing the extra information from the first
    line of an Order.
    
    Add a parseShopifyOrders function for decoding the contents of a file
    into a list with ShopifyOrders & their ShopifyLines.

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,12 +2,19 @@
 module Lib where
 
 
+import           Data.Char                      ( isDigit )
 import           Data.Csv
+import           Data.List.NonEmpty
 import           Data.Maybe
 import           Data.Scientific                ( Scientific )
 import           Data.Text                     as T
 
+import qualified Data.Vector                   as V
+import qualified Data.Text                     as T
+import qualified Data.ByteString.Lazy          as LBS
 
+
+-- SHOPIFY
 
 data ShopifyLine =
     ShopifyLine
@@ -24,6 +31,8 @@ data ShopifyLine =
         , slCountry :: T.Text
         , slSku :: T.Text
         , slName :: T.Text
+          -- Optional
+        , slQuantity :: Integer
         } deriving (Show, Read, Eq)
 
 
@@ -32,11 +41,11 @@ instance FromNamedRecord ShopifyLine where
         slDate       <- T.takeWhile (/= ' ') <$> r .: "Created at"
         slId         <- optionalPrefixed $ r .: "Id"
         slCustomerId <- prefixed $ r .: "Name"
-        lineQuantity <- r .: "Lineitem quantity"
+        slQuantity   <- r .: "Lineitem quantity"
         linePrice    <- r .: "Lineitem price"
-        let slLineTotal = lineQuantity * linePrice
+        let slLineTotal = fromInteger slQuantity * linePrice
         slShipRegion <- optional $ r .: "Shipping Province"
-        slShipZip    <- optional $ r .: "Shipping Zip"
+        slShipZip    <- fmap (T.filter isDigit) . optional $ r .: "Shipping Zip"
         slStreet     <- optional $ r .: "Shipping Address1"
         slCountry    <- optional $ r .: "Shipping Country"
         slSku        <- r .: "Lineitem sku"
@@ -46,3 +55,44 @@ instance FromNamedRecord ShopifyLine where
         optional         = fmap $ fromMaybe ""
         optionalPrefixed = fmap $ maybe "" ("wholesale-" <>)
         prefixed         = fmap ("wholesale-" <>)
+
+
+-- | This type contains the data that shopify only exports to the first
+-- row/line of each order in the export.
+data ShopifyOrder =
+    ShopifyOrder
+        { soId :: T.Text
+        , soShipRegion :: T.Text
+        , soShipZip :: T.Text
+        , soShipStreet :: T.Text
+        , soShipCountry :: T.Text
+        } deriving (Show, Read, Eq)
+
+
+parseShopifyOrders
+    :: LBS.ByteString -> Either String [(ShopifyOrder, [ShopifyLine])]
+parseShopifyOrders contents = case decodeByName contents of
+    Left err -> Left err
+    Right (_, sLines) ->
+        mapM processLines
+            $ groupWith (\l -> (slCustomerId l, slDate l))
+            $ V.toList sLines
+
+  where
+    processLines
+        :: NonEmpty ShopifyLine -> Either String (ShopifyOrder, [ShopifyLine])
+    processLines (first :| rest) =
+        if any (\f -> f first == "")
+               [slId, slShipRegion, slShipZip, slStreet, slCountry]
+            then
+                Left
+                    "Got blank id, region, zip, street, or country in first line of order."
+            else Right
+                ( ShopifyOrder { soId          = slId first
+                               , soShipRegion  = slShipRegion first
+                               , soShipZip     = slShipZip first
+                               , soShipStreet  = slStreet first
+                               , soShipCountry = slCountry first
+                               }
+                , first : rest
+                )
